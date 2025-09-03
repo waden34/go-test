b784841b8762d2c5b3881bfb38a509fe26d9ed01
Jeremiah Johnson <JeremiahJ@ncm.com>
2025-09-03 11:01:19 -0600

    50 Variadic functions

--- variadic_functions.go
+++ variadic_functions.go
@@ -6,6 +6,9 @@ func main() {
 	//func functionName(param1 type1, param2 type2, param3 ...type3) returnType
 
 	fmt.Println("The sum of these numbers is: ", sum(1, 2, 3, 5345, 364, 47))
+
+	numbers := []int{1, 2, 3, 4, 5, 9}
+	fmt.Println("The sum of the slice is: ", sum(numbers...))
 }
 
 func sum(numbers ...int) int {
